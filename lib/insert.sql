59df723d0739c0764b4fb075947e054687e8eb86
GregT <taylor.gregory2@gmail.com>
2022-06-18 19:22:07 -0600

    commit tree d31c4077c75d1b679eeeb614738641804d4bd9d1

---
+++ lib/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO bears (name, sex) VALUES
+('Mr. Chocolate', 'M'), ('Rowdy', 'M'), ('Tabitha', 'F'), ('Sergeant Brown', 'M'), ('Melissa', 'F'), ('Grinch', 'M'), ('Wendy', 'F'), (NULL, 'M');
